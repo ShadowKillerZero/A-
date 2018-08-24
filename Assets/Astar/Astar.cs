b5242522d948bdd3e2d8e327d748616ce6bf7f5f
ShadowKillerZero <15579136184@163.com>
2018-08-24 19:22:37 +0800

    AStar

--- Assets/Astar/Astar.cs
+++ Assets/Astar/Astar.cs
@@ -1,71 +1,239 @@
 ﻿using System;
 using System.Collections;
+using Priority_Queue;
 using System.Collections.Generic;
+using UnityEngine;
 
-public class Grid {
-   public int index;
+public enum GridType {
+    Wall,
+    Forest,
+    Ground,
+}
+public class Grid : MonoBehaviour {
+    public int x;
+    public int y;
+    public GridType currentType = GridType.Ground;
+    private void OnMouseDown()
+    {
+        Test.ChageType(this,this.currentType);
+    }
+    public Grid(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
 
-public class Graph
+public interface WeightedGraph<T>
 {
-    int[,] graph;
+    T[] Neighbors(T current);
+    int Cost(T next);
+}
+
+public class SquareGrid : WeightedGraph<Grid>
+{
+    private int width, height;
+    private Grid[,] edges;
+    private HashSet<Grid> walls = new HashSet<Grid>();
+    private HashSet<Grid> forests = new HashSet<Grid>();
+    private static readonly Grid[] Dirs = new[]
+    {
+        new Grid(1,0),
+        new Grid(0,-1),
+        new Grid(-1,0),
+        new Grid(0,1),
+    };
+    public int Width
+    {
+        get { return width; }   
+    }
+    public int Height {
+        get { return height; }
+    }
+    public SquareGrid(int width,int height)
+    {
+        this.width = width;
+        this.height = height;
+        edges = new Grid[width,height];
+    }
+
+    public void AddWall(Grid grid) {
+        walls.Add(grid);
+    }
+
+    public void RemoveWall(Grid grid) {
+        walls.Remove(grid);
+    }
+    public void AddForest(Grid grid)
+    {
+        forests.Add(grid);
+    }
+    public void RemoveForest(Grid grid)
+    {
+        forests.Remove(grid);
+    }
+
+    public void AddEdge(Grid grid)
+    {
+        if (Passable(grid)&& InBounds(grid))
+        {
+            edges[grid.x, grid.y] = grid;
+        }
+    }
+    public void RemoveEdge(Grid grid) {
+        if (Passable(grid) && InBounds(grid))
+        {
+            if (edges[grid.x, grid.y] != null)
+            {
+                edges[grid.x, grid.y] = null;
+            }
+        }
+    }
+
+    public bool Passable(Grid grid) {
+        return !walls.Contains(grid);
+    }
+
+    public bool InBounds(Grid id) {
+        return id.x >= 0 && id.x < width && 0 <= id.y && id.y < height;
+    }
+    public bool InBounds(int x,int y)
+    {
+        return x >= 0 && x < width && 0 <= y && y < height;
+    }
+    public int Cost(Grid next)
+    {
+        return forests.Contains(next) ? 5 : 1;
+    }
 
     public Grid[] Neighbors(Grid current)
     {
-        return null;
-    }
-    public int Cost(Grid first, Grid next) {
-        return graph[first.index, next.index];
+        List<Grid> neighbors = new List<Grid>();
+        for (int i = 0; i < Dirs.Length; i++)
+        {
+            if (InBounds(current.x + Dirs[i].x, current.y + Dirs[i].y))
+            {
+                Grid next = edges[current.x + Dirs[i].x, current.y + Dirs[i].y];
+                if (Passable(next))
+                {
+                    neighbors.Add(next);
+                }
+            }
+        }
+        return neighbors.ToArray();
     }
 }
-public class PriorityQueue<U,T>
+
+public class PriorityQueue<U,T> where T: IComparable<T>
 {
-    SortedList priorityQueue;
+   
+    SimplePriorityQueue<U,T> priorityQueue;
     public int Count {
         get { return priorityQueue.Count; }
     }
 
     public PriorityQueue()
     {
-        priorityQueue = new SortedList();
+        priorityQueue = new SimplePriorityQueue<U,T>();
     }
-    public T Push(U key,T value)
+    public U Push(U key, T value)
     {
-        priorityQueue.Add(key, value);
-        return value;
+        priorityQueue.Enqueue(key,value);
+        return key;
     }
-    public T Pop()
+    public U Pop()
     {
-        T first = (T)priorityQueue.GetByIndex(0);
-        priorityQueue.RemoveAt(0);
-        return first;
+        return priorityQueue.Dequeue();
     }
-    public T Peek()
+    public U Peek()
     {
-        return (T)priorityQueue.GetByIndex(0);
+        return priorityQueue.First;
     }
 }
 
 
 public class Astar {
-    Graph graph;
-    PriorityQueue<int,Grid> frontier;
-    
-    Grid[] came_from;
-    int[] cost_so_far;
-
-    int gridCount;
-    public Astar() {
-        came_from = new Grid[gridCount];
-        cost_so_far = new int[gridCount];
-        frontier = new PriorityQueue<int,Grid>();
+    SquareGrid graph;
+    PriorityQueue<Grid, double> frontier;  
+    private Dictionary<Grid,Grid> came_from;
+    private Dictionary<Grid,double> cost_so_far;
+    public Astar(SquareGrid graph) {
+        this.graph = graph;
     }
-    
-    public void ReadAllPath(Grid start,Grid goal)
+
+    public int Heuristic(Grid a, Grid b) {
+        return Math.Abs(a.x - b.x) + Math.Abs(a.y - b.y); 
+    }
+
+    //使用开始的实际距离来优先排列
+    //public void Dijkstra(Grid start, Grid goal)
+    //{
+    //    came_from = new Dictionary<Grid, Grid>();
+    //    cost_so_far = new Dictionary<Grid, double>();
+    //    frontier = new PriorityQueue<Grid, double>();
+    //    frontier.Push(start, 0);
+    //    came_from[start] = start;
+    //    cost_so_far[start] = 0;
+    //    while (frontier.Count > 0)
+    //    {
+    //        Grid current = frontier.Pop();
+    //        if (current == goal)
+    //            break;
+    //        Grid[] neighbors = graph.Neighbors(current);
+    //        for (int i = 0; i < neighbors.Length; i++)
+    //        {
+    //            Grid next = neighbors[i];
+    //            double new_cost = cost_so_far[current] + graph.Cost(next);
+    //            if (came_from.ContainsKey(next) || new_cost < cost_so_far[next])
+    //            {
+    //                cost_so_far[next] = new_cost;
+    //                double priority = new_cost;
+    //                frontier.Push(next, priority);
+    //                came_from[next] = current;
+    //            }
+    //        }
+    //    }
+    //}
+    //以离终点的距离作为优先级来排列
+    //public void GreedyBestFirstSearch(Grid start, Grid goal)
+    //{
+    //    came_from = new Dictionary<Grid, Grid>();
+    //    cost_so_far = new Dictionary<Grid, double>();
+    //    frontier = new PriorityQueue<Grid, double>();
+    //    frontier.Push(start, 0);
+    //    came_from[start] = start;
+    //    while (frontier.Count > 0)
+    //    {
+    //        Grid current = frontier.Pop();
+    //        if (current == goal)
+    //            break;
+    //        Grid[] neighbors = graph.Neighbors(current);
+    //        for (int i = 0; i < neighbors.Length; i++)
+    //        {
+    //            Grid next = neighbors[i];
+    //            if (came_from.ContainsKey(next))
+    //            {
+    //                int priority = Heuristic(goal,next);
+    //                frontier.Push(next, priority);
+    //                came_from[next] = current;
+    //            }
+    //        }
+    //    }
+    //}
+    public void AstarSearch(Grid start, Grid goal)
     {
-        frontier.Push(0,start);
-        came_from[start.index] = null;
-        cost_so_far[start.index] = 0;
+        came_from = new Dictionary<Grid, Grid>();
+        cost_so_far = new Dictionary<Grid, double>();
+        frontier = new PriorityQueue<Grid, double>();
+        //if (start != null)
+        //{
+        //    Debug.Log("start " + start.name);
+        //    Debug.Log("end " + goal.name);
+        //}
+        frontier.Push(start, 0);
+        came_from.Add(start, start);
+        //came_from[start] = start;
+        cost_so_far.Add(start, 0);
+        //cost_so_far[start] = 0;
         while (frontier.Count > 0)
         {
             Grid current = frontier.Pop();
@@ -75,28 +243,53 @@ public class Astar {
             for (int i = 0; i < neighbors.Length; i++)
             {
                 Grid next = neighbors[i];
-                int new_cost = cost_so_far[current.index] + graph.Cost(current, next);
-                if (came_from[next.index] == null || new_cost < cost_so_far[next.index])
+                double new_cost = cost_so_far[current] + graph.Cost(next);
+                if (!cost_so_far.ContainsKey(next) || new_cost < cost_so_far[next])
                 {
-                    cost_so_far[next.index] = new_cost;
-                    int priority = new_cost;
-                    frontier.Push(priority,next);
-                    came_from[next.index] = current;
+                    if (cost_so_far.ContainsKey(next))
+                    {
+                        cost_so_far[next] = new_cost;
+                    }
+                    else
+                        cost_so_far.Add(next, new_cost);
+                   
+                    double priority = new_cost + Heuristic(goal, next);
+                    frontier.Push(next, priority);
+                    if(came_from.ContainsKey(next))
+                        came_from[next] = current;
+                    else
+                        came_from.Add(next,current);
+
                 }
             }
         }
     }
 
-    public void GetShortestPath(Grid start,Grid goal)
+    public List<Grid> GetShortestPath(Grid start,Grid goal)
     {
+        if (!graph.Passable(goal) || !graph.Passable(start))
+        {
+            Debug.Log("不能形成路径");
+            return null;
+        }
+        AstarSearch(start, goal);
         Grid current = goal;
         List<Grid> path = new List<Grid>();
         while (current != start)
         {
             path.Add(current);
-            current = came_from[current.index];
+            if (current == null)
+            {
+                Debug.Log("current");
+            }
+            if (came_from == null)
+            {
+                Debug.Log("came_from");
+            }
+            current = came_from[current];
         }
         path.Add(start);
         path.Reverse();
+        return path;
     }
 }
---
+++ Assets/Test.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Priority_Queue;
+
+public class Test : MonoBehaviour {
+
+    public int length, weight;
+    static SquareGrid squareGrid;
+    Astar astar;
+    Grid start;
+    Grid end;
+    public static void ChageType(Grid grid,GridType gridType) {
+        switch (gridType)
+        {
+            case GridType.Wall:
+                grid.currentType = GridType.Ground;
+                squareGrid.RemoveWall(grid);
+                grid.GetComponent<MeshRenderer>().material.color = Color.white;
+                break;
+            case GridType.Forest:
+                grid.currentType = GridType.Wall;
+                squareGrid.RemoveForest(grid);
+                squareGrid.AddWall(grid);
+                grid.GetComponent<MeshRenderer>().material.color = Color.yellow;
+                break;
+            case GridType.Ground:
+                grid.currentType = GridType.Forest;
+                squareGrid.AddForest(grid);
+                grid.GetComponent<MeshRenderer>().material.color = Color.green;
+                break;
+            default:
+                break;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        //SimplePriorityQueue<string> simPriQueue = new SimplePriorityQueue<string>();
+        //simPriQueue.Enqueue("111", 1);
+        //simPriQueue.Enqueue("111", 1);
+        //simPriQueue.Enqueue("111", 1);
+        //while (simPriQueue.Count>0)
+        //{
+        //    Debug.Log(simPriQueue.Dequeue());
+        //}
+        Grid[,] Grids = new Grid[weight, length];
+        squareGrid = new SquareGrid(weight,length);
+        int count = 0;
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < weight; j++)
+            {
+                count++;
+                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                go.name = count.ToString();
+                go.transform.position = new Vector3(i, 0, j);
+                go.transform.localScale *= 0.7f;
+                go.GetComponent<MeshRenderer>().material.color = Color.white;
+                Grid grid = go.AddComponent<Grid>();
+                grid.x = i;grid.y = j;
+                squareGrid.AddEdge(grid);
+                if (i == 0 && j == 0)
+                {
+                    start = grid;
+                    grid.gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
+                }
+                if (i == 15&&j == 15)
+                {
+                    end = grid;
+                    grid.gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
+                }
+                Grids[i, j] = grid;
+                grid.currentType = GridType.Ground;
+            }
+        }
+        for (int i = 0; i < 100; i++)
+        {
+            int x = Random.Range(0, 19);
+            int y = Random.Range(0, 19);
+            if (x == 0 && y == 0)
+            {
+                continue;
+            }
+            if (x == 15 && y == 15)
+            {
+                continue;
+            }
+            Grid grid = Grids[x, y];
+            grid.currentType = GridType.Wall;
+            grid.gameObject.GetComponent<MeshRenderer>().material.color = Color.yellow;
+            squareGrid.AddWall(grid);
+        }
+        for (int i = 0; i < 50; i++)
+        {
+            int x = Random.Range(0, 19);
+            int y = Random.Range(0, 19);
+            if (x == 0 && y == 0)
+            {
+                continue;
+            }
+            if (x == 15 && y == 15)
+            {
+                continue;
+            }
+            Grid grid = Grids[x, y];
+            grid.currentType = GridType.Forest;
+            if (squareGrid.Passable(grid))
+            {
+                grid.gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
+                squareGrid.AddForest(grid);
+            }
+        }
+        astar = new Astar(squareGrid);
+    }
+
+    List<Grid> gridList;
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (gridList!=null)
+            {
+                for (int i = 0; i < gridList.Count; i++)
+                {
+                    
+                    Grid grid = gridList[i];
+                    if (grid.currentType == GridType.Ground)
+                    {
+                        Debug.Log(i);
+                        grid.gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+                    }
+                    else if (grid.currentType == GridType.Forest) {
+                        grid.gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
+                    }
+
+                }
+            }
+            gridList = astar.GetShortestPath(start, end);
+            for (int i = 0; i < gridList.Count; i++)
+            {
+                gridList[i].gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+        }
+	}
+}
